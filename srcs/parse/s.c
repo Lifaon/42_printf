0588018ce09f3e6d94bf339221b4344d29e3df23
mlantonn <meriadec.lantonnet@gmail.com>
2019-03-25 17:57:26 +0000

    NULL %s parameter

--- srcs/parse/s.c
+++ srcs/parse/s.c
@@ -6,11 +6,21 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 17:42:37 by mlantonn          #+#    #+#             */
-/*   Updated: 2019/03/21 21:25:53 by mlantonn         ###   ########.fr       */
+/*   Updated: 2019/03/25 18:53:06 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "conv.h"
+
+static void	print_null(t_param *param)
+{
+	add_char_to_buff(param, '(');
+	add_char_to_buff(param, 'n');
+	add_char_to_buff(param, 'u');
+	add_char_to_buff(param, 'l');
+	add_char_to_buff(param, 'l');
+	add_char_to_buff(param, ')');
+}
 
 void	s(t_param *param)
 {
@@ -21,15 +31,21 @@ void	s(t_param *param)
 	len = 0;
 	i = 0;
 	str = va_arg(param->ap, char *);
-	while (str[len])
-		++len;
+	if (str == NULL)
+		len = 6;
+	else
+		while (str[len])
+			++len;
 	if (param->preci >= 0 && len > param->preci)
 		len = param->preci;
 	if (len < param->width && !param->flag.minus)
 		while (len++ < param->width)
 			add_char_to_buff(param, ' ');
-	while (str[i] && (param->preci == -1 || i < param->preci))
-		add_char_to_buff(param, str[i++]);
+	if (str == NULL)
+		print_null(param);
+	else
+		while (str[i] && (param->preci == -1 || i < param->preci))
+			add_char_to_buff(param, str[i++]);
 	if (len < param->width && param->flag.minus)
 		while (len++ < param->width)
 			add_char_to_buff(param, ' ');
